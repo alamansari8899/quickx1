6a3b04d2e3d5ceda547cd1c0515b63a226c4b236
A.A <95412628+alamansari8899@users.noreply.github.com>
2025-02-06 14:25:06 +0530

    Add files via upload

---
+++ extraMenu.js
@@ -0,0 +1,12 @@
+var _extraMenu = function(t) {
+    let e = '<div style="position: absolute; top: 0%; left: 0%; width: 100vw; min-height: 100vh; background: white;">\n\t<div class="container-fluid card card-body">\n\t  \x3c!-- top navbar of logo and close button --\x3e\n\t  <div class="row mb-2" style="border-bottom: 1px solid lightgray;margin-top: -10px;position: sticky;top: 0px;background-color: white;z-index: 2010;"> \n\t\t\x3c!-- logo --\x3e\n\t\t<div id="" class="col-8 justify-content-start">\n\t\t<div class="" style="width:7rem; max-height: 40px;">\n\t\t  <svg fill-rule="evenodd" stroke="black" stroke-width="0.501" stroke-linejoin="bevel" stroke-miterlimit="10" font-family="Times New Roman" font-size="16" style="font-variant-ligatures:none;/*! text-align: left; 
+        function o(t) {
+        var o = e;
+        document.getElementById("i-additionalNavItemsBody").innerHTML += o
+    }
+    return {
+        addUI: function(t) {
+            o()
+        }
+    }
+};
